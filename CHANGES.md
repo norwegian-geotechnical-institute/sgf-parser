b59f208dbf6d36f432b1377d5de68477350d7c9f
cameliadguez <camelia.dguez@gmail.com>
2024-05-23 15:41:51 +0200

    Placeholder dissipation (#6)
    * Dissipation test  HM = 35 addition
    
    * Dissipation test  HM = 35 addition 2
    
    * Dissipation test  HM = 35 addition 3
    
    * Dissipation test  HM = 35 addition 4
    
    * Dissipation test  HM = 35 addition after first round of review
    
    * Dissipation test  HM = 35 addition after first round of review
    
    * Dissipation test  HM = 35 addition after first round of review
    
    ---------
    
    Co-authored-by: Camelia Dominguez Quintans <camelia.dominguez.quintans@ngi.no>

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,12 @@
 # NGI Python SGF Parser Package
 
+_2024-05-22_
+
+Version 0.0.1a2
+
+Addition of dissipation test method with limited support.
+
+
 _2024-04-03_
 
 Version 0.0.1a1 (Alpha Release)
---
+++ src/sgf_parser/models/method_dt.py
@@ -0,0 +1,33 @@
+from decimal import Decimal
+from typing import Literal
+
+from pydantic import Field
+
+from sgf_parser.models import MethodType, MethodData, Method
+
+
+class MethodDTData(MethodData):
+    """
+    Method Pore Pressure Dissipation Test (placeholder)
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    time: Decimal | None = Field(None, alias="AD", description="Elapsed time (s)")
+    u2: Decimal | None = Field(None, alias="AG", description="Shoulder pressure (kPa)")
+    depth: Decimal | None = Field(None, alias="D", description="Depth (m)")
+
+class MethodDT(Method):
+    """
+    Method Pore Pressure Dissipation Test (placeholder)
+    """
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    name: str = "DT"
+    method_type: Literal[MethodType.DT] = MethodType.DT
+    method_data_type: type[MethodDTData] = MethodDTData
+
+    method_data: list[MethodDTData] = []
